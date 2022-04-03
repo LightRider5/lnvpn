09e8aea8624ddfdaf1c77be83468dd53d1936d71
Christoph Bichlmeier <a11728738@unet.univie.ac.at>
2022-04-03 14:10:47 +0200

    react deployment finished

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>LN VPN</h1>
+       
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
